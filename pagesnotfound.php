e91b71dfd6dba99501c9281a95f1148c8edf03e0
leemyong pakvn <3759923+leemyongpakvn@users.noreply.github.com>
2023-04-04 12:17:09 +0700

    php-cs-fixer fix

--- pagesnotfound.php
+++ pagesnotfound.php
@@ -17,7 +17,6 @@
  * @copyright Since 2007 PrestaShop SA and Contributors
  * @license   https://opensource.org/licenses/AFL-3.0 Academic Free License version 3.0
  */
-
 if (!defined('_PS_VERSION_')) {
     exit;
 }

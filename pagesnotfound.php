908e5c1fa0a5d2640fe67a487f1d7685b12732b4
Daniel Hlavacek <daniel.hlavacek@hotmail.cz>
2023-05-05 14:52:24 +0200

    Update pagesnotfound.php

--- pagesnotfound.php
+++ pagesnotfound.php
@@ -36,7 +36,7 @@ class PagesNotFound extends Module
     {
         $this->name = 'pagesnotfound';
         $this->tab = 'administration';
-        $this->version = '2.0.2';
+        $this->version = '2.0.3';
         $this->author = 'PrestaShop';
         $this->need_instance = 0;
 

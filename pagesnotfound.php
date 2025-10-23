c23dd671853ea31d1f98c5711872b1290148198a
Thomas Nabord <thomas.nabord@prestashop.com>
2025-10-23 17:13:21 +0100

    Bump to v2.0.4

--- pagesnotfound.php
+++ pagesnotfound.php
@@ -36,7 +36,7 @@ class PagesNotFound extends Module
     {
         $this->name = 'pagesnotfound';
         $this->tab = 'administration';
-        $this->version = '2.0.3';
+        $this->version = '2.0.4';
         $this->author = 'PrestaShop';
         $this->need_instance = 0;
 
